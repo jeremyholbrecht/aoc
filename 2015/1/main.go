60d7f15b720064d79c4df404ee7e8406e0784571
jeremyholbrecht <griffley@proton.me>
2025-02-13 15:10:27 +0100

    solved part 1

--- 2015/1/main.go
+++ 2015/1/main.go
@@ -1,23 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "log"
+)
 
-const instructions  = ")())())"
+const path = "data.txt"
 
 func main(){
-    fmt.Println(calc_floor(instructions))
+    fmt.Println(calc_floor(path))
 }
 
-func calc_floor(parentheses string) int{
+func calc_floor(path string)int{
     floor := 0
+    parentheses, err := os.ReadFile(path)
+
+    if err != nil {
+        log.Fatal(err)
+    }
 
     for _, parenthesis := range parentheses{
-        if parenthesis  == '('{
+        if parenthesis == '('{
             floor += 1
         } else {
             floor -= 1
         }
     }
-
     return floor
 }
