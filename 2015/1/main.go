4c49b9275c63286c47644c27f4087ae2e97904b4
jeremyholbrecht <griffley@proton.me>
2025-02-13 15:35:05 +0100

    solved part two

--- 2015/1/main.go
+++ 2015/1/main.go
@@ -9,9 +9,11 @@ import (
 const path = "data.txt"
 
 func main(){
-    fmt.Println(calc_floor(path))
+   // fmt.Println(calc_floor(path))
+   find_position_basement(path)
 }
 
+//part 1
 func calc_floor(path string)int{
     floor := 0
     parentheses, err := os.ReadFile(path)
@@ -28,4 +30,29 @@ func calc_floor(path string)int{
         }
     }
     return floor
+}
+
+//part 2
+func find_position_basement(path string){
+    floor := 0
+    parentheses, err := os.ReadFile(path)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for i, parenthesis := range parentheses{
+        if parenthesis == '('{
+            floor += 1
+        } else {
+            floor -= 1
+        }
+
+        // we need to add 1 to the index because in this puzzle the first element is at index 1 and not 0
+        if floor == -1 {
+            fmt.Printf("%d basement reached \n", i + 1)
+
+        }
+    }
+
 }
