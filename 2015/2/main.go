9d4265802ba7b689a41bc831043a4d4654189317
jeremyholbrecht <griffley@proton.me>
2025-02-14 13:36:35 +0100

    feat: implemented a function to read text line by line from a .txt file

--- 2015/2/main.go
+++ 2015/2/main.go
@@ -3,10 +3,20 @@ package main
 import (
     "fmt"
     "sort"
+    "os"
+    "log"
+    "bufio"
 )
+
+const PATH = "data.txt"
 
 func main(){
     fmt.Println(calc_amount_wrapping_paper([]int{2,3,4}))
+
+    //TODO: convert each text line to a int slice
+    for _, line := range get_text(PATH){
+        fmt.Println(line)
+    }
 }
 
 
@@ -23,4 +33,24 @@ func calc_amount_wrapping_paper(dimensions []int) int{
     total := area + slack
 
     return total
+}
+
+func get_text(path string) []string{
+    file, err := os.Open(PATH)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+   scanner := bufio.NewScanner(file)
+   scanner.Split(bufio.ScanLines)
+   var text []string
+
+   for scanner.Scan(){
+      text = append(text, scanner.Text())
+   }
+
+   file.Close()
+
+   return text
 }
