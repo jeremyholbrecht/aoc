6c1af488d85f4150d6f1b89c454b30f07b64b93c
jeremyholbrecht <griffley@proton.me>
2025-02-14 12:28:45 +0100

    solved algo part of puzzle

--- 2015/2/main.go
+++ 2015/2/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "sort"
+)
 
 func main(){
-    fmt.Println("Hello, World")
+    fmt.Println(calc_amount_wrapping_paper([]int{2,3,4}))
+}
+
+
+// TODO: use data from the puzzle input file
+func calc_amount_wrapping_paper(dimensions []int) int{
+    // 2*l*w + 2*w*h + 2*h*l
+    area := 2 * dimensions[0] * dimensions[1] + 2 * dimensions[1] * dimensions[2] + 2 * dimensions[0] * dimensions[2]
+
+    // calc area shortest side
+    // sorts in ascending order
+    sort.Ints(dimensions)
+    slack := dimensions[0] * dimensions[1]
+
+    total := area + slack
+
+    return total
 }
